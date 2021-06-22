81731c3751ead45a8e3c5ab120b1d0ff5038c32d
Qian Qiao <qian.qiao@gmail.com>
2021-06-22 09:42:08 +0000

    Added tests for nesting.

--- __tests__/stamping-machine.ts
+++ __tests__/stamping-machine.ts
@@ -9,3 +9,63 @@ test('Stamping simple string-string objects', () => {
         { field: 'key1' }, { field: 'key2' }
     ]);
 });
+
+test('stamping nested objects should result in column spanning', () => {
+    expect(stamp({
+        key1: 'value1',
+        key2: {
+            key21: 'value21',
+            key22: 'value22'
+        }
+    })).toEqual(<ColDef[]>[
+        { field: 'key1' }, {
+            field: 'key2',
+            children: [
+                { field: 'key21' }, { field: 'key22' }
+            ]
+        }
+    ]);
+});
+
+test('stamping nested object should apply recurisively', () => {
+    expect(stamp({
+        key1: 'value1',
+        key2: {
+            key21: 'value21',
+            key22: 'value22'
+        },
+        key3: {
+            key31: {
+                key311: 'value311',
+                key312: 'value312'
+            },
+            key32: {
+                key321: 'value321',
+                key322: 'value322'
+            }
+        }
+    })).toEqual(<ColDef[]>[
+        { field: 'key1' }, {
+            field: 'key2',
+            children: [
+                { field: 'key21' }, { field: 'key22' }
+            ]
+
+        },
+        {
+            field: 'key3',
+            children: [
+                {
+                    field: 'key31', children: [
+                        { field: 'key311' }, { field: 'key312' }
+                    ]
+                },
+                {
+                    field: 'key32', children: [
+                        { field: 'key321' }, { field: 'key322' }
+                    ]
+                }
+            ]
+        }
+    ]);
+});
